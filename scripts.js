fd36f1a42d397b465dcb01cd10562ce03b725fbf
patrysia <patrycja@example.com>
2017-07-01 16:44:16 +0200

    Added files: index.html, scripts.js and style.css

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Scrpits of jQuery in exercise
+
+var span = $('span');
+var paragraphs = $('p');
+
+$('span:even').css('color', 'red');
+paragraphs.each(function(index, element) {
+	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+	$(element).append(button)
+	});
+
+$("button").click(function() {
+	alert($(this).attr("data-tmp"));
+});
